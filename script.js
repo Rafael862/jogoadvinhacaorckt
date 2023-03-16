cae6c87497562e8533d1f904686f61fe84a04d3f
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-03-15 21:43:31 -0300

    finalizado projeto!

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-const randomNumber = Math.round(Math.random() * 10)
 
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleTryClick(event){
-    event.preventDefault() //não faça o padrão - é o que está escrito
 
-    const inputNumber = document.querySelector('#inputNumber')
-
-    if(Number(inputNumber.value) == randomNumber){
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
-
-        document.querySelector('.screen2 h2').innerText = `Acertou em ${xAttempts} tentativas`
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
     }
+})
+
+
+function handleTryClick(event) {
+    event.preventDefault() // nao faca o padrao
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+    }
+
     inputNumber.value = ""
     xAttempts++
 }
 
-//eventos
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleClick)
-btnTry.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
+function handleResetClick() {
+    toggleScreen()
     xAttempts = 1
-})
+    randomNumber = Math.round(Math.random() * 10)
+}
 
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
